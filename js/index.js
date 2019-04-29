38e11be43e6a890d22eb27d18a96202fc455cd82
1473 <995069790@qq.com>
2019-04-29 10:04:17 +0800

    20190429cj

--- js/index.js
+++ js/index.js
@@ -1,41 +1,44 @@
-let last_path = null
+let last_path = null;
 /* 框架主页面路由注册开始 */
 spaRouters.map('/index', function(transition) {
   spaRouters.asyncFun('js/route/components/index-main.js', transition)
-})
+});
 spaRouters.map('/charge', function(transition) {
   spaRouters.asyncFun('js/route/components/charge-main.js', transition)
-})
+});
 spaRouters.map('/customer', function(transition) {
   spaRouters.asyncFun('js/route/components/customer-main.js', transition)
-})
+});
 spaRouters.map('/activity', function(transition) {
   spaRouters.asyncFun('js/route/components/activity-main.js', transition)
-})
+});
 spaRouters.map('/account', function(transition) {
   spaRouters.asyncFun('js/route/components/account-main.js', transition)
-})
+});
 /* 框架主页面路由注册结束 */
 
 /* 首页子页面路由注册开始 */
 spaRouters.map('/game-hall', function(transition) {
   spaRouters.asyncFun('js/route/components/game-hall.js', transition)
-})
+});
+spaRouters.map('/game-hall-item', function(transition) {
+    spaRouters.asyncFun('js/route/components/game-hall-item.js', transition)
+});
 /* 首页子页面路由注册结束 */
 
 /* 我的账户子页面路由注册开始 */
 spaRouters.map('/my-wallet', function(transition) {
   spaRouters.asyncFun('js/route/components/wallet.js', transition)
-})
+});
 spaRouters.map('/account-records', function(transition) {
   spaRouters.asyncFun('js/route/components/account-records.js', transition)
-})
+});
 spaRouters.map('/account-details', function(transition) {
   spaRouters.asyncFun('js/route/components/account-details.js', transition)
-})
+});
 spaRouters.map('/setting', function(transition) {
   spaRouters.asyncFun('js/route/components/setting.js', transition)
-})
+});
 /* 我的账户子页面路由注册结束 */
 
 /*spaRouters.map('/detail2',function(transition){
@@ -48,7 +51,7 @@ spaRouters.beforeEach(function(transition) {
     // 模拟切换之前延迟，比如说做个异步登录信息验证
     transition.next()
   }, 100)
-})
+});
 spaRouters.afterEach(function(transition) {
   let to_path = transition.path.substring(1, transition.path.length)
   $('#' + to_path).addClass('active')
@@ -57,23 +60,22 @@ spaRouters.afterEach(function(transition) {
   }
   last_path = to_path
   // 切换之后dosomething
-})
+});
 spaRouters.init()
 
 $(window).resize(function() {
   setHtmlFontSize()
-  console.log('font-size', $('html').css('font-size'))
-})
+});
 $(function() {
   setHtmlFontSize()
   //getOtherFontSize();
-})
+});
 // 设置根元素字体大小；num为控制常量
 let setHtmlFontSize = function() {
   let _width = document.documentElement.clientWidth
   let root_size = _width / 50
   $('html').css('font-size', root_size + 'px')
-}
+};
 let getOtherFontSize = function() {
   $('.size-20').css('font-size', (20 * 50) / 750 + 'rem')
   $('.size-24').css('font-size', (24 * 50) / 750 + 'rem')
@@ -81,4 +83,4 @@ let getOtherFontSize = function() {
   $('.size-32').css('font-size', (32 * 50) / 750 + 'rem')
   $('.size-36').css('font-size', (36 * 50) / 750 + 'rem')
   $('.size-40').css('font-size', (40 * 50) / 750 + 'rem')
-}
+};
--- js/route/components/game-hall.js
+++ js/route/components/game-hall.js
@@ -1,20 +1,66 @@
 SPA_RESOLVE_INIT = function(transition, sigal) {
     $('#footer').hide();
-    $('#header').html('\n' +
-        '       <a href="#/index" class="back">\n' +
+    let type = transition.query.type;
+    let title = '';
+    let title_common = '<a href="#/index" class="back">\n' +
         '         <svg class="icon" aria-hidden="true">\n' +
         '           <use xlink:href="#icon-fanhui"></use>\n' +
         '         </svg>\n' +
-        '       </a>\n' +
-        '       <span class="game-hall-title size-36">炫彩大厅</span>');
+        '       </a>';
+    if(type == 1) {
+        title = title_common + '<span class="game-hall-title size-36">炫彩大厅</span>';
+    }
+    if(type == 2) {
+        title = title_common + '<span class="game-hall-title size-36">幻彩大厅</span>';
+    }
+    if(type == 3) {
+        title = title_common + '<span class="game-hall-title size-36">瑞彩大厅</span>';
+    }
+    $('#header').html(title);
     $('#header').css('justify-content', 'flex-start');
     $('.game-hall-title').css('flex-grow', '1');
     $('.game-hall-title').css('text-align', 'center');
     $('.back').css('color', '#fff');
     $('#main').html('\n' +
         '    <div class="game-hall-content">\n' +
-        '        <div class="room pre primary"></div>\n' +
-        '        <div class="room pre middle"></div>\n' +
-        '        <div class="room senior"></div>\n' +
+        '      <a href="#/game-hall-item">\n' +
+        '        <div class="room pre primary">\n' +
+        '          <button class="text first">赔率说明</button>\n' +
+        '          <p class="text second hall-tag1">底注： <strong>1</strong></p>\n' +
+        '          <p class="text second hall-tag2">准入： <strong>0</strong></p>\n' +
+        '          <span class="text third">          \n' +
+        '           <svg class="icon" aria-hidden="true">\n' +
+        '             <use xlink:href="#icon-zhanghu1"></use>\n' +
+        '           </svg>\n' +
+        '           <strong>9739</strong>\n' +
+        '          </span>\n' +
+        '        </div>\n' +
+        '      </a>\n' +
+        '      <a href="#/game-hall-item">\n' +
+        '        <div class="room pre middle">\n' +
+        '          <button class="text first">赔率说明</button>\n' +
+        '          <p class="text second hall-tag1">底注： <strong>100</strong></p>\n' +
+        '          <p class="text second hall-tag2">准入： <strong>50000</strong></p>\n' +
+        '          <span class="text third">          \n' +
+        '           <svg class="icon" aria-hidden="true">\n' +
+        '             <use xlink:href="#icon-zhanghu1"></use>\n' +
+        '           </svg>\n' +
+        '           <strong>9739</strong>\n' +
+        '          </span>\n' +
+        '        </div>\n' +
+        '      </a>\n' +
+        '      <a href="#/game-hall-item">\n' +
+        '        <div class="room senior">\n' +
+        '          <button class="text first">赔率说明</button>\n' +
+        '          <p class="text second hall-tag1">底注： <strong>300</strong></p>\n' +
+        '          <p class="text second hall-tag2">准入： <strong>100000</strong></p>\n' +
+        '          <span class="text third">          \n' +
+        '           <svg class="icon" aria-hidden="true">\n' +
+        '             <use xlink:href="#icon-zhanghu1"></use>\n' +
+        '           </svg>\n' +
+        '           <strong>9739</strong>\n' +
+        '          </span>\n' +
+        '        </div>\n' +
+        '      </a>\n' +
         '    </div>');
 };
