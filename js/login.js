75836f4f5471915dafe454493ff982836de0108d
1473 <995069790@qq.com>
2019-05-07 14:14:50 +0800

    20190507cj

--- js/login.js
+++ js/login.js
@@ -1,21 +1,24 @@
 (function() {
-  $('.login-submit').on('click', () => {
-    let user_num = $('.user').val();
-    let password = $('.password').val();
-    let data = {
-      user_num: user_num,
-      password: password
-    };
-    $.jsonAjax(API.LOGIN_API, 'POST', data)
-      .then(function(data) {
-        window.location.href = '#/index';
-        /* if (data.status == true) {
+  $(function () {
+      $('.login-submit').on('click', () => {
           window.location.href = '#/index';
-        } */
-        console.log('data', data);
-      })
-      .catch(function(error) {
-        console.log('11111', error.status);
+          /*let user_num = $('.user').val();
+          let password = $('.password').val();
+          let data = {
+            user_num: user_num,
+            password: password
+          };*/
+          /*$.jsonAjax(API.LOGIN_API, 'POST', data)
+            .then(function(data) {
+              window.location.href = '#/index';
+              /!* if (data.status == true) {
+                window.location.href = '#/index';
+              } *!/
+              console.log('data', data);
+            })
+            .catch(function(error) {
+              console.log('11111', error.status);
+            });*/
       });
   });
 })();
