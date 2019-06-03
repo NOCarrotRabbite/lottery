f77f94591abdb447a007f9b94900882796d5a2a7
chen <806275054@qq.com>
2019-06-03 17:25:52 +0800

    20190603-17.25 cjx

--- js/set-password.js
+++ js/set-password.js
@@ -6,5 +6,26 @@
     $.inputClear($('.old-pwd'), $('.old-pwd').next());
     $.inputClear($('.new-pwd'), $('.new-pwd').next());
     $.inputClear($('.repetition-pwd'), $('.repetition-pwd').next());
+    let user_num = localStorage.getItem('tel');
+    $('.set-password-submit').on('click', function() {
+      let data = {
+        user_num: user_num,
+        old_password: $('.old-pwd').val(),
+        new_password: $('.new-pwd').val()
+      };
+      $.jsonAjax(API.UP_PASSWORD_API, 'POST', data)
+        .then(function(res) {
+          console.log(res);
+          if (res.status == true) {
+            $.messageBox(res.message, 600);
+            window.location.href = '#/login';
+          } else {
+            $.messageBox(res.message, 600);
+          }
+        })
+        .catch(function(error) {
+          console.log(error.status);
+        });
+    });
   });
 })();
